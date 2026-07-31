20453df2cfaffa6dbb08135c0b89f7eb62baa462
Rajas-S <dr.rajas.sharma@gmail.com>
2026-07-31 11:06:34 +0100

    cleaned up code

--- index.js
+++ index.js
@@ -19,21 +19,21 @@ let fps;
 // -------------------------------------
 
 function setup() {
-  frameRate(2000)
   createCanvas(WIDTH, HEIGHT);
+  noStroke();
   // initalise n random particles with random attributes
-  for(i=0;i<n;i++){
-    x = i*WIDTH/(n);
-    y = random(0,HEIGHT/4);
-    r = random(1,4);
-    m = r*r;
-    vx = random(-100,80);
-    vy = random(-100,80)
-    append(p,new Particle(r,m,x,y,vx,vy,0,0,1))
+  for(let i=0;i<n;i++){
+    let x = i*WIDTH/(n);
+    let y = random(0,HEIGHT/4);
+    let r = random(1,4);
+    let m = r*r;
+    let vx = random(-100,80);
+    let vy = random(-100,80)
+    p.push(new Particle(r,m,x,y,vx,vy,0,0,1))
   }
 
   n+=1;
-  append(p,new Particle(50,2500,200,350,0,0,0,0,1))
+  p.push(new Particle(50,2500,200,350,0,0,0,0,1))
 
 }
 
@@ -41,30 +41,38 @@ function draw() {
 
     background(220);
     
+    // particles check and collide
     for(let j=0;j<n;j++){
-      
-      if(j<n/4){fill(255,0,0)}
-      else{fill(0,0,255)}
-      
-      // each particle checks collision with every other particle 
-      // => (make more efficient using grid system?)
       for(let k=j+1;k<n;k++){
         if(p[j].CheckCollision(p[k])){
               p[j].Collide(p[k])
-            }
-          }
-          
-          p[j].CheckWallCollision();
-          
-        // find time taken since the particle last updated
-        currenttime = Date.now()/1000;
-        dt = currenttime-lasttime;
-        
-        p[j].update(dt);
-        p[j].draw(); 
-        
+        }
       }
-      fps = 1/(currenttime-lasttime);
-      lasttime=currenttime;
+    }
+
+
+      //particles check wall collision
+    for(let i=0;i<n;i++){
+       p[i].CheckWallCollision();
+    }
+
+      // particles update positions and velocities
+    currenttime = Date.now()/1000;
+    dt = currenttime-lasttime;
+
+    for(let i=0;i<n;i++){
+       p[i].update(dt);
+    }
+
+      // draw each particle
+    for(let i=0;i<n;i++){
+      fill(0,0,255);
+      if(i<n/4){fill(255,0,0);}
+       p[i].draw(); 
+    }
+
+
+   fps = 1/dt;
+   lasttime=currenttime;
 }
 
