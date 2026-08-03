03d97b7b4f919c57fa98d2ec1f84f4225127194a
Rajas-S <dr.rajas.sharma@gmail.com>
2026-08-03 09:57:29 +0100

    updated conditions

--- index.js
+++ index.js
@@ -11,15 +11,19 @@ const SCREENX1 = 300, SCREENX2 = 1200, SCREENY1 = 0, SCREENY2 = HEIGHT
 
 let SIMSPEED = 1;
 let restitution = 0.9;
+let gridshow = 0;
+let showparticlecolour = 0;
+let particlerect = 1;
 
-const MAXPARTICLERADIUS = 30;
+const MAXPARTICLERADIUS = 2;
+const PARTICLECOLOURVALUE = 180;
 
 let pause = 0;
 
-let n = 4500;
-let p = [];
+let n = 15000;
+let p = Array(n);
 
-let lasttime = Date.now()/1000;
+let lasttime = performance.now()/1000;
 let currenttime;
 let dt;
 let fps;
@@ -38,24 +42,26 @@ function setup() {
   noStroke();
   // initalise n random particles with random attributes
   for(let i=0;i<n;i++){
-    let x = 900*Math.round(random(0,1))+300;
-    let y = 500*Math.round(random(0,1))+100;
-    let r = random(0.1,2.5);
+    //let x = 900*Math.round(random(0,1))+300;
+    //let y = 500*Math.round(random(0,1))+100;
+    let x = random(SCREENX1,SCREENX2);
+    let y = random(SCREENY1,SCREENY2);
+    let r = random(1.7,1.7);
     let m = r*r;
-    let vx = random(-150,0);
-    let vy = random(-100,0)
-    p.push(new Particle(r,m,x,y,vx,vy,0,0))
+    let vx = random(-150,150);
+    let vy = random(-150,150);
+    p[i]=new Particle(r,m,x,y,vx,vy,0,100);
   }
 
-  n+=1;
+  // n+=1;
   
-  let x = random(0,WIDTH);
-  let y = random(0,HEIGHT);
-  let r = random(30,30);
-  let m = r*r;
-  let vx = random(-50,50);
-  let vy = random(-50,50)
-  p.push(new Particle(r,m,x,y,vx,vy,0,0))
+  // let x = random(0,WIDTH);
+  // let y = random(0,HEIGHT);
+  // let r = random(50,50);
+  // let m = r*r;
+  // let vx = random(-50,50);
+  // let vy = random(-50,50);
+  // p.push(new Particle(r,m,800,50,0,200,0,0))
 
 
   // initialise gui
@@ -94,23 +100,25 @@ function main(){
   
     // draw each particle
     for(let i=0;i<n;i++){
-       p[i].draw(); 
+       p[i].draw(showparticlecolour,particlerect); 
     }
 }
 function draw() {
-  currenttime = Date.now()/1000;
+  currenttime = performance.now()/1000;
   dt = currenttime-lasttime;
   fps = 1/dt;
   lasttime=currenttime;
 
   main();
   GUI();
-  for(let x=0;x<numX;x++){
-    for(let y=0;y<numY;y++){
-      stroke(255);
-      strokeWeight(0.1);
-      noFill();
-      rect(x*MAXPARTICLERADIUS+SCREENX1,y*MAXPARTICLERADIUS+SCREENY1,MAXPARTICLERADIUS,MAXPARTICLERADIUS);
+  if(gridshow){
+    for(let x=0;x<numX;x++){
+      for(let y=0;y<numY;y++){
+        stroke(255);
+        strokeWeight(0.1);
+        noFill();
+        rect(x*MAXPARTICLERADIUS+SCREENX1,y*MAXPARTICLERADIUS+SCREENY1,MAXPARTICLERADIUS,MAXPARTICLERADIUS);
+      }
     }
   }
 }
@@ -149,11 +157,13 @@ function SpatialPartitioning(){
         let i1 = clampMin(i-numX,0);
         let i2 = clamp(i+1-numX,0,POSALEN);
 
-        for(let j=posA[clampMin(i-1-numX,0)];j<posA[i1];j++){if(p[iA[k]].CheckCollision(p[iA[j]])){p[iA[k]].Collide(p[iA[j]]);}}
-        for(let j=posA[i1];j<posA[i2];j++){if(p[iA[k]].CheckCollision(p[iA[j]])){p[iA[k]].Collide(p[iA[j]]);}}
-        for(let j=posA[i2];j<posA[clamp(i+2-numX,0,POSALEN)];j++){if(p[iA[k]].CheckCollision(p[iA[j]])){p[iA[k]].Collide(p[iA[j]]);}}
-        for(let j=posA[clampMin(i-1,0)];j<posA[i];j++){if(p[iA[k]].CheckCollision(p[iA[j]])){p[iA[k]].Collide(p[iA[j]]);}}
-        for(let j=posA[i];j<posA[clampMax(i+1,POSALEN)];j++){if(p[iA[k]].CheckCollision(p[iA[j]])){p[iA[k]].Collide(p[iA[j]]);}}
+        const particleK = p[iA[k]];
+
+        for(let j=posA[clampMin(i-1-numX,0)];j<posA[i1];j++){particleK.Collide(p[iA[j]]);}
+        for(let j=posA[i1];j<posA[i2];j++){particleK.Collide(p[iA[j]]);}
+        for(let j=posA[i2];j<posA[clamp(i+2-numX,0,POSALEN)];j++){particleK.Collide(p[iA[j]]);}
+        for(let j=posA[clampMin(i-1,0)];j<posA[i];j++){particleK.Collide(p[iA[j]]);}
+        for(let j=posA[i];j<posA[clampMax(i+1,POSALEN)];j++){particleK.Collide(p[iA[j]]);}
 
       }
     }
