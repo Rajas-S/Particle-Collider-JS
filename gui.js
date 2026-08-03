44d175297c8e723b3b4a16f7fcd0a84883f0604e
Rajas-S <dr.rajas.sharma@gmail.com>
2026-08-03 09:57:15 +0100

    updated gui

--- gui.js
+++ gui.js
@@ -1,6 +1,6 @@
 const PADDING = 10;
-const GRABRADIUS = 100;
-const SENSITIVITY = 2;
+let GRABRADIUS = 100;
+let SENSITIVITY = 2;
 let x_prev, y_prev;
 let stored_simspeed;
 
@@ -8,7 +8,10 @@ function initGUI(){
     pauseButton = new Button(30,20,50,20,"Pause",16,255,0,51,pausefunc);
     startButton = new Button(30,50,50,20,"Start",16,0,230,84,startfunc);
     simSpeed = new Slider(30,90,90,10,"Simulation Speed",16,50,50,50,2,1,0);
-    restitution_slider = new Slider(30,160,90,10,"Constant Restitution",16,50,50,50,1.3,1,0.03);
+    restitution_slider = new Slider(30,160,90,10,"Constant of Restitution",16,50,50,50,1.3,0.974,0.025);
+    grabradius_slider = new Slider(30,230,90,10,"Grab Radius",16,50,50,50,200,100,0);
+    sensitivity_slider = new Slider(30,300,90,10,"Sensitivity",16,50,50,50,5,2,0);
+    
 
     x_prev = mouseX; y_prev = mouseY;
 
@@ -43,6 +46,12 @@ function GUI(){
     if(!pause){SIMSPEED=simSpeed.click();}
     restitution_slider.draw();
     restitution=restitution_slider.click();
+
+    grabradius_slider.draw();
+    GRABRADIUS = grabradius_slider.click()
+
+    sensitivity_slider.draw();
+    SENSITIVITY = sensitivity_slider.click();
 }
 
 class Button{
